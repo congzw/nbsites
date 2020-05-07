2c484440a1af479d76db46158d7ada11ba9c4244
congzw <2981810300@qq.com>
2020-05-07 18:26:48 +0800

    add common code

---
+++ src/NbSites.Common.Test/MyJsonHelperSpec.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace NbSites.Common
+{
+    [TestClass]
+    public class MyJsonHelperSpec
+    {
+        [TestMethod]
+        public void DeserializeObject_String_ShouldOk()
+        {
+            var myJsonHelper = new MyJsonHelper();
+            var serializeObject = myJsonHelper.SerializeObject("ABC", true);
+            serializeObject.ShouldEqual("ABC");
+
+            myJsonHelper.DeserializeObject("ABC", "ddd").ShouldEqual("ABC");
+            myJsonHelper.DeserializeObject(null, "ddd").ShouldEqual("ddd");
+            myJsonHelper.DeserializeObject("", "ddd").ShouldEqual("ddd");
+            myJsonHelper.DeserializeObject(" ", "ddd").ShouldEqual("ddd");
+        }
+
+        [TestMethod]
+        public void DeserializeObject_String_Ext_ShouldOk()
+        {
+            var myJsonHelper = new MyJsonHelper();
+
+            myJsonHelper.DeserializeObject<string>("ABC", "ddd").ShouldEqual("ABC");
+            myJsonHelper.DeserializeObject<string>(null, "ddd").ShouldEqual("ddd");
+            myJsonHelper.DeserializeObject<string>("", "ddd").ShouldEqual("ddd");
+            myJsonHelper.DeserializeObject<string>(" ", "ddd").ShouldEqual("ddd");
+        }
+
+        [TestMethod]
+        public void ParseXElementAsDynamic_Single_Element_ShouldOk()
+        {
+            var myJsonHelper = new MyJsonHelper();
+            var loadXml = @"<Content DeviceIP=""192.168.1.10"">
+                <Task BurnStatus=""0"">
+                </Task>
+            </Content>";
+
+            var xDoc = XDocument.Parse(loadXml);
+            var root = xDoc.Elements().First();
+            var theObject = myJsonHelper.ParseXElementAsDynamic(root);
+
+            JsonConvert.SerializeObject((object)theObject, Formatting.Indented).Log();
+
+            var theContent = theObject.Content;
+            string deviceIP = theContent.DeviceIP;
+            deviceIP.Log();
+            deviceIP.ShouldEqual("192.168.1.10");
+
+            var theTask = theContent.Task;
+            string burnStatus = theTask.BurnStatus;
+            burnStatus.Log();
+        }
+        
+        [TestMethod]
+        public void ParseXElementAsDynamic_Array_Elements_ShouldOk()
+        {
+            var myJsonHelper = new MyJsonHelper();
+            var loadXml = @"<Content DeviceIP=""192.168.1.10"">
+                <Task BurnStatus=""0"">
+                </Task>
+                <Task BurnStatus=""1"">
+                </Task>
+            </Content>";
+
+
+            var xDoc = XDocument.Parse(loadXml);
+            var root = xDoc.Elements().First();
+            var theObject = myJsonHelper.ParseXElementAsDynamic(root);
+
+            JsonConvert.SerializeObject((object)theObject, Formatting.Indented).Log();
+
+            var theContent = theObject.Content;
+            string deviceIP = theContent.DeviceIP;
+            deviceIP.Log();
+            deviceIP.ShouldEqual("192.168.1.10");
+            
+            var theTask = theContent.Task;
+            string burnStatus = theTask[0].BurnStatus;
+            burnStatus.Log();
+            string burnStatus2 = theTask[1].BurnStatus;
+            burnStatus2.Log();
+        }
+    }
+}
---
+++ src/NbSites.Common/MyJsonHelper.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Dynamic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+
+namespace NbSites.Common
+{
+    public interface IMyJsonHelper
+    {
+        string SerializeObject(object value, bool indented);
+        object DeserializeObject(string json, object defaultValue);
+    }
+
+    public class MyJsonHelper : IMyJsonHelper
+    {
+        public string SerializeObject(object value, bool indented)
+        {
+            if (value is string strValue)
+            {
+                return strValue;
+            }
+            var formatting = indented ? Formatting.Indented : Formatting.None;
+            var serializeObject = JsonConvert.SerializeObject(value, formatting);
+            return serializeObject;
+        }
+
+        public object DeserializeObject(string json, object defaultValue)
+        {
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return defaultValue;
+            }
+            if (defaultValue is string strValue)
+            {
+                if (string.IsNullOrWhiteSpace(json))
+                {
+                    return strValue;
+                }
+                return json;
+            }
+            return JsonConvert.DeserializeObject(json);
+        }
+
+        #region for extensions
+
+        private static readonly MyJsonHelper _instance = new MyJsonHelper();
+        public static Func<IMyJsonHelper> Resolve = () => _instance;
+
+        #endregion
+    }
+
+    public static class MyJsonHelperExtensions
+    {
+        public static T DeserializeObject<T>(this IMyJsonHelper helper, string json, T defaultValue)
+        {
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return defaultValue;
+            }
+            if (typeof(T) == typeof(string))
+            {
+                if (string.IsNullOrWhiteSpace(json))
+                {
+                    return defaultValue;
+                }
+                return json.As<T>();
+            }
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public static dynamic ParseXElementAsDynamic(this IMyJsonHelper helper, XElement rootNode)
+        {
+            string jsonText = JsonConvert.SerializeXNode(rootNode, Formatting.Indented);
+            var withoutATSign = Regex.Replace(jsonText, "(?<=\")(@)(?!.*\":\\s )",
+                string.Empty, RegexOptions.IgnoreCase);
+            dynamic dyn = JsonConvert.DeserializeObject<ExpandoObject>(withoutATSign);
+            return dyn;
+        }
+
+        public static dynamic ParseXmlAsDynamic(this IMyJsonHelper helper, string xmlContent)
+        {
+            var xDoc = XDocument.Parse(xmlContent);
+            return helper.ParseXElementAsDynamic(xDoc.Elements().First());
+        }
+
+        public static dynamic ParseXmlFileAsDynamic(this IMyJsonHelper helper, string xmlFile)
+        {
+            var xDoc = XDocument.Load(xmlFile);
+            return helper.ParseXElementAsDynamic(xDoc.Elements().First());
+        }
+
+        private static T As<T>(this string value)
+        {
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
