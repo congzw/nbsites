98739f67f42973ceeeed270eec0dd983b47daf3f
congzw <2981810300@qq.com>
2020-05-07 18:40:48 +0800

    add AreaUrlHelperSpec

---
+++ src/NbSites.Common.Test/AreaUrlHelperSpec.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NbSites.Common.Web;
+
+namespace NbSites.Common
+{
+    [TestClass]
+    public class AreaUrlHelperSpec
+    {
+        [TestMethod]
+        public void CreateAreaContent_Normal_ShouldOk()
+        {
+            var areaContentArgs = AreaContentArgs.Create().WithArea("Admin").WithContent("~/Content/css/foo.css");
+            var areaUrlHelper = new AreaUrlHelper();
+            areaUrlHelper.CreateAreaContent(areaContentArgs).ShouldEqual("~/Areas/Admin/Content/css/foo.css");
+        }
+    }
+}
