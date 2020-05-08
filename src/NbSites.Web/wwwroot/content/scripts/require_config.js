bd93db854cc8373bd685fb6a1e2bca4afac2903b
congzw <2981810300@qq.com>
2020-05-08 17:41:09 +0800

    add demo layout

---
+++ src/NbSites.Web/wwwroot/content/scripts/require_config.js
@@ -0,0 +1,9 @@
+require.config({
+    paths: {
+        'jquery': '/Content/libs/jquery/jquery-2.1.0'
+        , 'toastr': '/Content/libs/toastr/toastr'
+        , 'bootstrap': '/Content/scripts/bootstrap'
+    },
+    shim: {
+    }
+});
---
+++ src/NbSites.Web/wwwroot/content/scripts/showScriptsLoading.js
@@ -0,0 +1,36 @@
+﻿require(['jquery', 'toastr'], function ($, toastr) {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-bottom-full-width",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "-1",
+        "extendedTimeOut": "-1",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut",
+        closeMethod: false
+    };
+
+    var toastMessage = function (msg) {
+        toastr.info(msg, 'async load scripts:');
+    };
+
+    $(function () {
+
+        var scriptsInfo = "";
+        //$("script").each(function () {
+        //    scriptsInfo += "<p>" + this.src + "</p>";
+        //});
+        $("script[async]").each(function () {
+            scriptsInfo += "<p>" + this.src + "</p>";
+        });
+        toastMessage(scriptsInfo);
+    });
+});
