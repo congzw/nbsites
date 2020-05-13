77fdb5cf95bc9e8f5d9015022a88ff690bcdd1a6
congzw <2981810300@qq.com>
2020-05-13 14:01:16 +0800

    add MessageResult and spec template

---
+++ src/NbSites.Common.Test/_Spec.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NbSites.Common
+{
+    [TestClass]
+    public class _Spec
+    {
+        [TestMethod]
+        public void Sth_When_Should()
+        {
+        }
+    }
+}
